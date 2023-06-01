7ee801b16ce5d6186e76d6e50bdf21e2bc307b9a
nyggus <marcin.kozak@contractors.roche.com>
2023-06-01 12:10:10 +0200

    Increment version (v0.6.1)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_requirements = {
 
 setuptools.setup(
     name="perftester",
-    version="0.6.0",
+    version="0.6.1",
     author="Nyggus",
     author_email="nyggus@gmail.com",
     description="Lightweight performance testing in Python",
